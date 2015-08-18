f12121d449d3e1f36b6d2b448add78d719421aa7
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-18 11:03:37 -0700

    updated telescope, add toggle for 'flag'

--- packages/custom-package/lib/client/templates/custom_flag_post.js
+++ packages/custom-package/lib/client/templates/custom_flag_post.js
@@ -5,47 +5,29 @@ Telescope.downvoteItem = function (collection, itemId, user) {
   var item = collection.findOne(itemId);
 
   // make sure user has rights to downvote first
-  if (!user || !Users.can.vote(user, true)  || hasFlaggedItem(item, user))
+  if (!user || !Users.can.vote(user, true)  || hasDownvotedItem(item, user))
     return false;
 
   // ------------------------------ Callbacks ------------------------------ //
 
   // run all downvote callbacks on item successively
-  item = Telescope.callbacks.run("flag", item);
+  item = Telescope.callbacks.run("downvote", item);
 
   // ----------------------------------------------------------------------- //
 
   var votePower = getVotePower(user);
 
   // in case user is downvoting a previously upvoted item, cancel upvote first
-  // Telescope.cancelUpvote(collection, item, user);
+  Telescope.cancelUpvote(collection, item, user);
 
   // Votes & Score
   var result = collection.update({_id: item && item._id, downvoters: { $ne: user._id }},{
     $addToSet: {downvoters: user._id},
-    // $inc: {downvotes: 1, baseScore: -votePower},
+    $inc: {downvotes: 1, baseScore: -votePower},
     $set: {inactive: false}
   });
 
   if (result > 0) {
-    function flagNotification (post) {
-
-      var adminIds = _.pluck(Users.find({'isAdmin': true}, {fields: {_id:1}}).fetch(), '_id');
-      var notifiedUserIds = _.pluck(Users.find({'telescope.notifications.posts': true}, {fields: {_id:1}}).fetch(), '_id');
-      var notificationData = {
-        post: _.pick(post, '_id', 'userId', 'title', 'url', 'vote')
-      };
-
-      // remove post author ID from arrays
-      adminIds = _.without(adminIds, post.userId);
-      notifiedUserIds = _.without(notifiedUserIds, post.userId);
-
-      
-        // if post is pending, only notify admins
-    Herald.createNotification(adminIds, {courier: 'newFlaggedPost', data: flagNotification});
-
-    }
-    Telescope.callbacks.add("postFlagAsync", flagNotification);
     // Add item to list of downvoted items
     var vote = {
       itemId: item._id,
@@ -55,7 +37,7 @@ Telescope.downvoteItem = function (collection, itemId, user) {
     addVote(user._id, vote, collectionName, 'down');
 
     // extend item with baseScore to help calculate newScore
-    // item = _.extend(item, {baseScore: (item.baseScore - votePower)});
+    item = _.extend(item, {baseScore: (item.baseScore - votePower)});
     Telescope.updateScore({collection: collection, item: item, forceUpdate: true});
 
     // if the item is being upvoted by its own author, don't give karma
@@ -68,13 +50,6 @@ Telescope.downvoteItem = function (collection, itemId, user) {
 
     // ----------------------------------------------------------------------- //
   }
-  // console.log(collection.findOne(item._id));
+  console.log(collection.findOne(item._id));
   return true;
 };
-
-Meteor.methods({
-  flagPost: function (postId) {
-    check(postId, String);
-    return Telescope.downvoteItem.call(this, Posts, postId);
-  }
-});
