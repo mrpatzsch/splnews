78bc20ea5c836aaf318ca7db33c111e7fb13b317
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-17 17:08:34 -0700

    try to get flag as downvote numbers

--- packages/custom-package/lib/client/templates/custom_flag_post.js
+++ packages/custom-package/lib/client/templates/custom_flag_post.js
@@ -1,4 +1,4 @@
-Telescope.flagItem = function (collection, itemId, user) {
+Telescope.downvoteItem = function (collection, itemId, user) {
 
   user = typeof user === "undefined" ? Meteor.user() : user;
   var collectionName = collection._name.slice(0,1).toUpperCase()+collection._name.slice(1);
@@ -28,45 +28,53 @@ Telescope.flagItem = function (collection, itemId, user) {
   });
 
   if (result > 0) {
-    function flagSubmitNotification (post) {
+    function flagNotification (post) {
 
       var adminIds = _.pluck(Users.find({'isAdmin': true}, {fields: {_id:1}}).fetch(), '_id');
+      var notifiedUserIds = _.pluck(Users.find({'telescope.notifications.posts': true}, {fields: {_id:1}}).fetch(), '_id');
       var notificationData = {
-        post: _.pick(post, '_id', 'userId', 'title', 'url', 'downvotes')
+        post: _.pick(post, '_id', 'userId', 'title', 'url', 'vote')
       };
 
       // remove post author ID from arrays
       adminIds = _.without(adminIds, post.userId);
+      notifiedUserIds = _.without(notifiedUserIds, post.userId);
 
-      if (!!notifiedUserIds.length) {
-        // if post is approved, notify everybody
-        Herald.createNotification(notifiedUserIds, {courier: 'newPost', data: notificationData});
-      }
+      
+        // if post is pending, only notify admins
+    Herald.createNotification(adminIds, {courier: 'newFlaggedPost', data: flagNotification});
 
     }
-  Telescope.callbacks.add("postSubmitAsync", postSubmitNotification);
+    Telescope.callbacks.add("postFlagAsync", flagNotification);
     // Add item to list of downvoted items
-    // var vote = {
-    //   itemId: item._id,
-    //   votedAt: new Date(),
-    //   power: votePower
-    // };
-    // addVote(user._id, vote, collectionName, 'down');
+    var vote = {
+      itemId: item._id,
+      votedAt: new Date(),
+      power: votePower
+    };
+    addVote(user._id, vote, collectionName, 'down');
 
-    // // extend item with baseScore to help calculate newScore
+    // extend item with baseScore to help calculate newScore
     // item = _.extend(item, {baseScore: (item.baseScore - votePower)});
-    // Telescope.updateScore({collection: collection, item: item, forceUpdate: true});
+    Telescope.updateScore({collection: collection, item: item, forceUpdate: true});
 
-    // // if the item is being upvoted by its own author, don't give karma
-    // if (item.userId !== user._id)
-    //   modifyKarma(item.userId, votePower);
+    // if the item is being upvoted by its own author, don't give karma
+    if (item.userId !== user._id)
+      modifyKarma(item.userId, votePower);
 
-    // // --------------------- Server-Side Async Callbacks --------------------- //
+    // --------------------- Server-Side Async Callbacks --------------------- //
 
-    // Telescope.callbacks.runAsync("downvoteAsync", item);
+    Telescope.callbacks.runAsync("downvoteAsync", item);
 
     // ----------------------------------------------------------------------- //
   }
-  console.log(collection.findOne(item._id));
+  // console.log(collection.findOne(item._id));
   return true;
 };
+
+Meteor.methods({
+  flagPost: function (postId) {
+    check(postId, String);
+    return Telescope.downvoteItem.call(this, Posts, postId);
+  }
+});
