797e6dc3aa94ca41cae294cc0a47ab6c3bf5fd7c
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-26 06:28:33 -0700

    removed other packages

--- packages/telescope-posts/lib/client/templates/post_page.js
+++
@@ -1,9 +0,0 @@
-Template.post_page.helpers({
-  isPending: function () {
-    return this.post && this.post.status === Posts.config.STATUS_PENDING;
-  }
-});
-
-Template.post_page.rendered = function(){
-  $('body').scrollTop(0);
-};
