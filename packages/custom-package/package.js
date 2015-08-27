34320c7427ad48d1c3a939296e5724dd63dfffdd
Andrew Patzsch <mrpatzsch@gmail.com>
2015-08-27 15:03:03 -0700

    fix up/down vote in comments

--- packages/custom-package/package.js
+++ packages/custom-package/package.js
@@ -38,6 +38,7 @@ Package.onUse(function (api) {
     'lib/client/templates/custom_post_author.html',
     'lib/client/templates/custom_search.html',
     'lib/client/templates/custom_meta.html',
+    'lib/client/templates/custom_comment_item.html',
     'lib/client/stylesheets/custom.scss',
   ], ['client']);
 
